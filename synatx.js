d26fd22e058fc47454974f9c95c79b7a5dbebd22
Abdullah Shahzad <abdullahshahzadnjr@gmail.com>
2026-09-01 14:18:13 +0500

    Update code

--- synatx.js
+++ synatx.js
@@ -1,1 +1,14 @@
 console.log("Hello world");
+let x=5;
+const y=10;
+var z=15
+
+let n=42
+let f=3.14
+let s="hello"
+let b=true;
+let nothing = null;
+let notDefined
+
+let name="abdullah"
+console.log(`Hello, ${name}!`);
